51edc19b4f077322ebcbd3cf2429023ea44c741e
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2025-02-03 00:39:27 +0530

    Setting Up The Flutter Project, Setting Up Themes, Folder Structure, Setting Up Routes

---
+++ flutter_amazon_clone/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_amazon_clone/constants/global_variables.dart';
+import 'package:flutter_amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:flutter_amazon_clone/router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Amazon clone',
+        theme: ThemeData(
+            scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+            colorScheme: const ColorScheme.light(
+                primary: GlobalVariables.secondaryColor),
+            appBarTheme: const AppBarTheme(
+                elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Hello'),
+            ),
+            body: Column(children: [
+              const Center(child: Text('Flutter Demo Home Page')),
+              Builder(
+                builder: (context) {
+                  return ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, AuthScreen.routeName);
+                      },
+                      child: Text('Click'));
+                }
+              ),
+            ])));
+  }
+}
