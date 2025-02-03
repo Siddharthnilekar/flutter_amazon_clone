9eef63c3f5c800f38d4feb2c2abbf9eaa595efbd
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2025-02-03 17:28:13 +0530

    Create your First API

--- flutter_amazon_clone/server/index.js
+++ flutter_amazon_clone/server/index.js
@@ -1,11 +1,16 @@
 console.log("helllo, world!");
 
-const express = require('express');
+const express = require("express");
 const PORT = 3000;
 
 const app = express();
 
 //CREATING AN API
-app.listen(PORT, "0.0.0.0", () =>{
-    console.log(`connexted at port ${PORT}`);
+app.get("/hello-word", (req, res) => {
+  res.json({hi: "hello world!"});
+});
+// GET, PUT, DELETE, UPDATE => CRUD
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connexted at port ${PORT}`);
 });
