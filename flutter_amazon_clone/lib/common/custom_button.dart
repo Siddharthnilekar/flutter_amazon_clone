217594f739692bb79c866b4c10323ca42a403b57
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2025-02-03 14:05:17 +0530

    Auth Screen UI

---
+++ flutter_amazon_clone/lib/common/custom_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_amazon_clone/constants/global_variables.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  const CustomButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: GlobalVariables.secondaryColor,
+        minimumSize: const Size(double.infinity, 50),
+      ),
+      child: Text(text,
+      style: const TextStyle(
+        color: Colors.white,
+      ),),
+      
+    );
+  }
+}
