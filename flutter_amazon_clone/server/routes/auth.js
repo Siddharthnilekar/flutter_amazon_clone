fdc683223f8da3d08dfe5bfca1159051936aa06b
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2025-02-03 19:26:36 +0530

    Signup Route

--- flutter_amazon_clone/server/routes/auth.js
+++ flutter_amazon_clone/server/routes/auth.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const authRouter = express.Router();
 
-authRouter.get('/user' , (req, res) => {
-    res.json({msg: "siddharth"})
-})
+authRouter.post('/api/signup', (req, res) => {
+    const{name, email, password} = req.body;
+    //get the data from client
+    //post that data in database
+    //return that data to the user
+});
 
 module.exports = authRouter;
